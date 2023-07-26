b2a49817a0837062ad8b7868e3ee2390b06a0952
peron <peron_ap@icloud.com>
2023-07-26 12:58:24 +0800

    feat: add website title

--- routes/index.tsx
+++ routes/index.tsx
@@ -4,23 +4,34 @@ import { Message } from "$/utils/types.ts";
 import { getCookies } from "$std/http/cookie.ts";
 import { RECEIVER_KEY } from "$/utils/constants.ts";
 import ReceiverKeyInputCard from "$/components/ReceiverKeyInputCard.tsx";
+import { Head } from "$fresh/src/runtime/head.ts";
 
 export default async function MessagesViewer(req: Request) {
   const { receiverKey = "" } = getCookies(req.headers);
 
   if (receiverKey !== RECEIVER_KEY) {
     return (
-      <div className="space-y-4 p-8 max-w-lg mx-auto">
-        <ReceiverKeyInputCard />
-      </div>
+      <>
+        <Head>
+          <title>SMS Forwarder Server | Login</title>
+        </Head>
+        <div className="space-y-4 p-8 max-w-lg mx-auto">
+          <ReceiverKeyInputCard />
+        </div>
+      </>
     );
   }
 
   const messages: Message[] = await listMessages();
 
   return (
-    <div className="space-y-4 p-8 max-w-lg mx-auto">
-      {messages.map((message) => <MessageCard message={message} />)}
-    </div>
+    <>
+      <Head>
+        <title>SMS Forwarder Server | Messages</title>
+      </Head>
+      <div className="space-y-4 p-8 max-w-lg mx-auto">
+        {messages.map((message) => <MessageCard message={message} />)}
+      </div>
+    </>
   );
 }
